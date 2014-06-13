1e51b1a6b56018770b878bdb968c80ac1b0e2b37
NeLk <nelk42@gmail.com>
2014-06-13 23:26:30 +0100

    New passing test

--- wrap.js
+++ wrap.js
@@ -2,12 +2,17 @@ var expect = require('chai').expect;
 
 function wrap(wordToWrap, width) {
     if(!wordToWrap) return '';
+    return wordToWrap;
 }
 
 describe("First test", function(){
 
-   it("wrap empty string", function(){
-       expect(wrap('', 0)).to.eql('');
-   });
+    it("wrap empty string", function(){
+        expect(wrap('', 0)).to.eql('');
+    });
+
+    it("word does not need wrapping", function(){
+        expect(wrap('hello', 5)).to.eql('hello');
+    });
 
 });
